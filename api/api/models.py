49a5e5e904278a71c13d0326aad338e93fdd27cb
Isaac Botwe <isaacbotwe7@gmail.com>
2020-08-12 11:34:31 +0000

    miscellaneous

--- api/api/models.py
+++ api/api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Disease(models.Model):
+    name: models.CharField(max_length=100, unique=True)
+    added: models.DateField(auto_now_add=True)
