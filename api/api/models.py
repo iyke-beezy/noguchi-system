344db46fb8a5dfa8e55250fa44163e8316dd226f
Isaac Botwe <isaacbotwe7@gmail.com>
2020-08-12 12:36:32 +0000

    model updates

--- api/api/models.py
+++ api/api/models.py
@@ -4,4 +4,11 @@ from django.db import models
 class Disease(models.Model):
     name: models.CharField(max_length=100, unique=True)
     added: models.DateField(auto_now_add=True)
-    
+
+class User(models.model):
+    TYPE = (
+        ('co', 'contributor'),
+        ('ad', 'admin')
+    )
+    name: models.CharField(max_length=100, unique=True)
+    userType: models.CharField(max_length=2, choices=TYPE)
