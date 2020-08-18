ad131cf68a660bd16c58f01c5492e880fb58aec9
MuniruIssah <munirisaah2092@gmail.com>
2020-08-18 05:57:27 -0700

    .

--- noguchi/noguchi-web/src/pages/Login/login.js
+++ noguchi/noguchi-web/src/pages/Login/login.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import OrgForm from '../../components/form';
-
-
+import './styles.css'
 
 const OrgLogin =()=>{
+    const image=require('../../assets/whiteGhana.png')
     return(
         <div style={{height:'100vh',backgroundColor:'whitesmoke',display:'flex',justifyContent:'center',alignItems:'center'}}>
-            <OrgForm/>
+            <div className='formwrap'>
+                
+                 <OrgForm/>                
+            </div>
+
         </div>
     );
 }
 export default OrgLogin;
+
