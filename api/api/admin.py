3f59db402d0978763342aade42d7852d2d581426
= <=>
2020-11-09 05:28:11 +0000

    changing the api to nodejs

--- api/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Disease, User, Contributor
-# Register your models here.
-
-admin.site.register(Disease)
-admin.site.register(User)
-admin.site.register(Contributor)
