e9d7b4da5b043232b0a19f10a3954e6fec99236c
MuniruIssah <imuniru@st.ug.edu.gh>
2021-06-13 11:49:35 +0000

    reverting back yh

--- noguchi/noguchi-web/src/components/signUpCard.js
+++ noguchi/noguchi-web/src/components/signUpCard.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React, { useState } from "react";
 import { Input, Card, Space, Button, Avatar, Select, Form } from 'antd';
 import "antd/dist/antd.css";
@@ -28,6 +29,15 @@ import './components.css'
 import {UserOutlined,LockOutlined,MailOutlined} from '@ant-design/icons';
 
 import axios from 'axios'
+=======
+import React, {useState} from "react";
+import { Input, Card, Button , Form} from 'antd';
+import "antd/dist/antd.css";
+import './components.css'
+import {UserOutlined,LockOutlined,MailOutlined} from '@ant-design/icons';
+
+import axios from 'axios'
+>>>>>>> parent of 5b2abbb (reverted)
 
 const SignUpCard =({onClick,...props})=>{
   const [fullname,setFullname]=useState('');
--- noguchi/noguchi-web/src/pages/Login/userlogin.js
+++ noguchi/noguchi-web/src/pages/Login/userlogin.js
@@ -3,8 +3,12 @@ import { UserForm } from '../../components/form';
 import SignUpCard from '../../components/signUpCard';
 import './styles.css'
 
+<<<<<<< HEAD:noguchi/noguchi-web/src/pages/Login/userlogin.js
 const UserLoginForm =()=>{
     const image=require('../../assets/whiteGhana.png')
+=======
+const OrgLogin =()=>{
+>>>>>>> parent of 5b2abbb (reverted):noguchi/noguchi-web/src/pages/Login/login.js
     const [login,setLogin]=useState(true);
     return(
         <div className='login' style={{minHeight:'100vh',height:'auto',display:'flex',justifyContent:'center',alignItems:'center'}}>
